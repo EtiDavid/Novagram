ecd4c46d0740d29dc2ab83b0ee952dc4352ad5be
Eti David <etidavid26@gmail.com>
2026-03-02 21:38:30 +0100

    fix: return only account_id from aws_caller_identity

--- terraform/modules/policies/outputs.tf
+++ terraform/modules/policies/outputs.tf
@@ -2,6 +2,8 @@ output "secrets_policy_arn" {
   value = aws_iam_policy.backend_secrets_policy.arn
 }
 
+
+
 output "aws_account_id" {
-  value = data.aws_caller_identity.current
+  value = data.aws_caller_identity.current.account_id
 }
