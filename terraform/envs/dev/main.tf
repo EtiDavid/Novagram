32f9f0fefd0af13332184825dc08007403d025ee
Eti David <etidavid26@gmail.com>
2026-03-02 23:00:14 +0100

    fix: task-definition upload issue

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -128,7 +128,7 @@ module "backend_ecs_service" {
   task_name = "backend"
   cluster_id = module.ecs_cluster.cluster_id
   task_definition_arn = module.backend_task_definition.task_definition_arn
-  desired_count = 0
+  desired_count = 2
   security_group_ids = [module.security_groups.backend_sg_id]
   subnet_ids = module.network.public_subnet_ids
   target_group_arn = module.backend_target_group.aws_lb_target_group_arn
@@ -165,7 +165,7 @@ module "frontend_task_definition" {
   task_name = "frontend"
   cluster_id = module.ecs_cluster.cluster_id
   task_definition_arn = module.frontend_task_definition.task_definition_arn
-  desired_count = 0
+  desired_count = 1
   security_group_ids = [module.security_groups.frontend_sg_id]
   subnet_ids = module.network.public_subnet_ids
   target_group_arn = module.frontend_target_group.aws_lb_target_group_arn
